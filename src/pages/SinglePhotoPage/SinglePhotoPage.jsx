80d31e815abef066c473f2fcf8e00e0dffdb2c72
Daniela Conde <daniela_conde2@hotmail.com>
2025-02-20 20:09:23 -0700

    add useParams and API to fetch object user is currently on

--- src/pages/SinglePhotoPage/SinglePhotoPage.jsx
+++ src/pages/SinglePhotoPage/SinglePhotoPage.jsx
@@ -1,15 +1,34 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import Header from "../../components/Header/Header";
 import Card from "../../components/Card/Card";
 import Footer from "../../components/Footer/Footer";
 import "./SinglePhotoPage.scss";
 
 function SinglePhotoPage() {
+  const [photo, setPhoto] = useState(null);
+  const params = useParams(); //this is the hook that returns the object the user is currently on
+  const api_key = "api_key=13273d8d-5377-423f-a5e5-50a3b0904a29";
+
+  const loadSinglePhoto = async () => {
+    const response = await axios.get(
+      `https://unit-3-project-c5faaab51857.herokuapp.com/photos/${params.id}?${api_key}`
+    );
+    setPhoto(response.data);
+  };
+
+  useEffect(() => {
+    loadSinglePhoto();
+  }, []);
+
+  if (photo === null) {
+    return <p>Loading Photo...</p>;
+  }
   return (
     <>
-      <Header />
-      <Card />
-      {/* Form to be put here */}
-      <Footer />
+      {/* <Header /> */}
+      {/* create a card component */}
+      {/* create a form and post axios request */}
     </>
   );
 }
